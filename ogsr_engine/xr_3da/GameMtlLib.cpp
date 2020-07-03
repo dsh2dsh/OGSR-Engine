00dc9eade854fa84c843ab3e5dcf80c44899c3e1
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2020-07-03 16:34:10 +0200

    Немного переделал загрузку материалов
    1. Если встречается описание пары материалов, которое уже было
    добавлено, то ругаемся в лог и игнорируем.
    
    2. Автоматически генерируем описание пар материалов, которые не были
    загружены. На всякий случай.

--- ogsr_engine/xr_3da/GameMtlLib.cpp
+++ ogsr_engine/xr_3da/GameMtlLib.cpp
@@ -62,68 +62,94 @@ void SGameMtl::Load(IReader& fs)
     	fDensityFactor		= fs.r_float();
 }
 
-void CGameMtlLibrary::Load()
-{
-	string_path			name;
-	if (!FS.exist(name,	_game_data_,GAMEMTL_FILENAME)){
-    	Log				("! Can't find game material file: ",name);
-    	return;
+
+void CGameMtlLibrary::Load() {
+  string_path name;
+  if ( !FS.exist( name, _game_data_, GAMEMTL_FILENAME ) ) {
+    Log( "! Can't find game material file: ", name );
+    return;
+  }
+
+  R_ASSERT( material_pairs.empty() );
+  R_ASSERT( materials.empty() );
+
+  IReader* F  = FS.r_open( name );
+  IReader& fs = *F;
+
+  R_ASSERT( fs.find_chunk( GAMEMTLS_CHUNK_VERSION ) );
+  u16 version = fs.r_u16();
+  if ( GAMEMTL_CURRENT_VERSION != version ) {
+    Log( "CGameMtlLibrary: invalid version. Library can't load." );
+    FS.r_close( F );
+    return;
+  }
+
+  R_ASSERT( fs.find_chunk( GAMEMTLS_CHUNK_AUTOINC ) );
+  material_index = fs.r_u32();
+  material_pair_index = fs.r_u32();
+
+  materials.clear();
+  material_pairs.clear();
+
+  IReader* OBJ = fs.open_chunk( GAMEMTLS_CHUNK_MTLS );
+  if ( OBJ ) {
+    u32 count;
+    for ( IReader* O = OBJ->open_chunk_iterator( count ); O; O = OBJ->open_chunk_iterator( count, O ) ) {
+      SGameMtl* M = xr_new<SGameMtl>();
+      M->Load( *O );
+      materials.push_back( M );
     }
+    OBJ->close();
+  }
 
-    R_ASSERT			(material_pairs.empty());
-    R_ASSERT			(materials.empty());
+  material_count = (u32)materials.size();
+  material_pairs_rt.resize( material_count * material_count, nullptr );
+  int last_pair_id = -1;
 
-	IReader*	F		= FS.r_open(name);
-    IReader& fs			= *F;
-
-    R_ASSERT(fs.find_chunk(GAMEMTLS_CHUNK_VERSION));
-    u16 version			= fs.r_u16();
-    if (GAMEMTL_CURRENT_VERSION!=version){
-        Log				("CGameMtlLibrary: invalid version. Library can't load.");
-		FS.r_close		(F);
-    	return;
-    }
-
-    R_ASSERT(fs.find_chunk(GAMEMTLS_CHUNK_AUTOINC));
-    material_index		= fs.r_u32();
-    material_pair_index	= fs.r_u32();
-
-    materials.clear		();
-    material_pairs.clear();
-
-    IReader* OBJ 		= fs.open_chunk(GAMEMTLS_CHUNK_MTLS);
-    if (OBJ) {
-        u32				count;
-        for (IReader* O = OBJ->open_chunk_iterator(count); O; O = OBJ->open_chunk_iterator(count,O)) {
-        	SGameMtl*	M = xr_new<SGameMtl> ();
-	        M->Load		(*O);
-        	materials.push_back(M);
+  OBJ = fs.open_chunk( GAMEMTLS_CHUNK_MTLS_PAIR );
+  if ( OBJ ) {
+    u32 count;
+    for ( IReader* O = OBJ->open_chunk_iterator( count ); O; O = OBJ->open_chunk_iterator( count, O ) ) {
+      SGameMtlPair* M = xr_new<SGameMtlPair>( this );
+      M->Load( *O );
+      int idx0 = GetMaterialIdx( M->mtl0 ) * material_count + GetMaterialIdx( M->mtl1 );
+      if ( SGameMtlPair* S = material_pairs_rt[ idx0 ] ) {
+        Msg( "! [%s]: ignore [%u][%s] found [%u][%s]", __FUNCTION__, M->ID, M->dbg_Name(), S->ID, S->dbg_Name() );
+        xr_delete( M );
+      }
+      else {
+        material_pairs.push_back( M );
+        material_pairs_rt[ idx0 ] = M;
+        if ( M->mtl0 != M->mtl1 ) {
+          int idx1 = GetMaterialIdx( M->mtl1 ) * material_count + GetMaterialIdx( M->mtl0 );
+          material_pairs_rt[ idx1 ] = M;
         }
-        OBJ->close		();
+        if ( M->ID > last_pair_id )
+          last_pair_id = M->ID;
+      }
     }
+    OBJ->close();
+  }
 
-    OBJ 				= fs.open_chunk(GAMEMTLS_CHUNK_MTLS_PAIR);
-    if (OBJ){
-        u32				count;
-        for (IReader* O = OBJ->open_chunk_iterator(count); O; O = OBJ->open_chunk_iterator(count,O)) {
-        	SGameMtlPair* M	= xr_new<SGameMtlPair> (this);
-	        M->Load		(*O);
-        	material_pairs.push_back(M);
+  for ( auto* M1 : materials ) {
+    for ( auto* M2 : materials ) {
+      int idx0 = GetMaterialIdx( M1->GetID() ) * material_count + GetMaterialIdx( M2->GetID() );
+      if ( !material_pairs_rt[ idx0 ] ) {
+        SGameMtlPair* M = xr_new<SGameMtlPair>( this );
+        last_pair_id++;
+        M->ID   = last_pair_id;
+        M->mtl0 = M1->GetID();
+        M->mtl1 = M2->GetID();
+        //Msg( "* [%s]: auto generate [%d][%s]", __FUNCTION__, M->ID, M->dbg_Name() );
+        material_pairs.push_back( M );
+        material_pairs_rt[ idx0 ] = M;
+        if ( M1->GetID() != M2->GetID() ) {
+          int idx1 = GetMaterialIdx( M2->GetID() ) * material_count + GetMaterialIdx( M1->GetID() );
+          material_pairs_rt[ idx1 ] = M;
         }
-        OBJ->close		();
+      }
     }
-
-#ifndef _EDITOR
-	material_count		= (u32)materials.size();
-    material_pairs_rt.resize(material_count*material_count,0);
-    for (GameMtlPairIt p_it=material_pairs.begin(); material_pairs.end() != p_it; ++p_it){
-		SGameMtlPair* S	= *p_it;
-    	int idx0		= GetMaterialIdx(S->mtl0)*material_count+GetMaterialIdx(S->mtl1);
-    	int idx1		= GetMaterialIdx(S->mtl1)*material_count+GetMaterialIdx(S->mtl0);
-	    material_pairs_rt[idx0]=S;
-	    material_pairs_rt[idx1]=S;
-    }
-#endif
+  }
 
 /*
 	for (GameMtlPairIt p_it=material_pairs.begin(); material_pairs.end() != p_it; ++p_it){
@@ -133,8 +159,10 @@ void CGameMtlLibrary::Load()
 		}
 	}
 */
-	FS.r_close		(F);
+
+  FS.r_close( F );
 }
+
 
 #ifdef GM_NON_GAME
 SGameMtlPair::~SGameMtlPair		()
