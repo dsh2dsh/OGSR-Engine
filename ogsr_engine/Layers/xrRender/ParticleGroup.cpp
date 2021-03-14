519f6d6f8e1781da4d616f3a231417da1ee5aeb2
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2021-03-14 16:24:42 +0100

    Поправил отступы для наглядности

--- ogsr_engine/Layers/xrRender/ParticleGroup.cpp
+++ ogsr_engine/Layers/xrRender/ParticleGroup.cpp
@@ -439,46 +439,47 @@ CParticleGroup::~CParticleGroup()
 	items.clear();
 }
 
-void CParticleGroup::OnFrame(u32 u_dt)
-{
-	if (m_Def&&m_RT_Flags.is(flRT_Playing)){
-        float ct	= m_CurrentTime;
-        float f_dt	= float(u_dt)/1000.f;
-        for (CPGDef::EffectVec::const_iterator e_it=m_Def->m_Effects.begin(); e_it!=m_Def->m_Effects.end(); e_it++){	
-            if ((*e_it)->m_Flags.is(CPGDef::SEffect::flEnabled)){
-            	VERIFY				(items.size()==m_Def->m_Effects.size());
-                SItem& I			= items[e_it-m_Def->m_Effects.begin()];
-                if (I.IsPlaying()){
-                    if ((ct<=(*e_it)->m_Time1)&&(ct+f_dt>=(*e_it)->m_Time1))	
-                        I.Stop((*e_it)->m_Flags.is(CPGDef::SEffect::flDefferedStop));
-                }else{
-                    if (!m_RT_Flags.is(flRT_DefferedStop))
-                        if ((ct<=(*e_it)->m_Time0)&&(ct+f_dt>=(*e_it)->m_Time0))	
-                            I.Play();
-                }
-            }
+void CParticleGroup::OnFrame( u32 u_dt ) {
+  if ( m_Def && m_RT_Flags.is( flRT_Playing ) ) {
+    float ct   = m_CurrentTime;
+    float f_dt = float(u_dt)/1000.f;
+    for ( CPGDef::EffectVec::const_iterator e_it = m_Def->m_Effects.begin(); e_it != m_Def->m_Effects.end(); e_it++ ) {
+      if ( (*e_it)->m_Flags.is( CPGDef::SEffect::flEnabled ) ) {
+        VERIFY( items.size() == m_Def->m_Effects.size() );
+        SItem& I = items[ e_it - m_Def->m_Effects.begin() ];
+        if ( I.IsPlaying() ) {
+          if ( ct <= (*e_it)->m_Time1 && ct + f_dt >= (*e_it)->m_Time1 )
+            I.Stop( (*e_it)->m_Flags.is( CPGDef::SEffect::flDefferedStop ) );
         }
-        m_CurrentTime 	+= f_dt;
-        if ((m_CurrentTime>m_Def->m_fTimeLimit)&&(m_Def->m_fTimeLimit>0.f))
-            if (!m_RT_Flags.is(flRT_DefferedStop)) Stop(true);
-
-        bool bPlaying = false;
-        Fbox box; box.invalidate();
-        for (SItemVecIt i_it=items.begin(); i_it!=items.end(); i_it++) 
-        	i_it->OnFrame(u_dt,*m_Def->m_Effects[i_it-items.begin()],box,bPlaying);
-
-        if (m_RT_Flags.is(flRT_DefferedStop)&&!bPlaying){
-            m_RT_Flags.set		(flRT_Playing|flRT_DefferedStop,FALSE);
+        else{
+          if ( !m_RT_Flags.is( flRT_DefferedStop ) )
+            if ( ct <= (*e_it)->m_Time0 && ct + f_dt >= (*e_it)->m_Time0 )
+              I.Play();
         }
-        if (box.is_valid()){
-        	vis.box.set			(box);
-			vis.box.getsphere	(vis.sphere.P,vis.sphere.R);
-		}
-	} else {
-		vis.box.set			(m_InitialPosition,m_InitialPosition);
-		vis.box.grow		(EPS_L);
-		vis.box.getsphere	(vis.sphere.P,vis.sphere.R);
-	}
+      }
+    }
+    m_CurrentTime += f_dt;
+    if ( m_CurrentTime > m_Def->m_fTimeLimit && m_Def->m_fTimeLimit > 0.f )
+      if ( !m_RT_Flags.is( flRT_DefferedStop ) )
+        Stop( true );
+
+    bool bPlaying = false;
+    Fbox box; box.invalidate();
+    for ( SItemVecIt i_it = items.begin(); i_it != items.end(); i_it++ )
+      i_it->OnFrame( u_dt, *m_Def->m_Effects[ i_it - items.begin() ], box, bPlaying );
+
+    if ( m_RT_Flags.is( flRT_DefferedStop ) && !bPlaying )
+      m_RT_Flags.set( flRT_Playing | flRT_DefferedStop, FALSE );
+    if ( box.is_valid() ) {
+      vis.box.set( box );
+      vis.box.getsphere( vis.sphere.P, vis.sphere.R );
+    }
+  }
+  else {
+    vis.box.set( m_InitialPosition, m_InitialPosition );
+    vis.box.grow( EPS_L );
+    vis.box.getsphere( vis.sphere.P, vis.sphere.R );
+  }
 }
 
 void CParticleGroup::UpdateParent(const Fmatrix& m, const Fvector& velocity, BOOL bXFORM)
