c558994601f06b2fba4bfa1f3fe2890e10fae2f6
Krodin <16670637+KRodinn@users.noreply.github.com>
2021-12-04 13:18:08 +0100

    Добавил для аномалий параметр `DestroyAfterBlowout`, чтобы можно было удалить мины после их взрыва

--- ogsr_engine/xrGame/CustomZone.cpp
+++ ogsr_engine/xrGame/CustomZone.cpp
@@ -329,6 +329,8 @@ void CCustomZone::Load(LPCSTR section)
 
 	m_ef_anomaly_type			= pSettings->r_u32(section,"ef_anomaly_type");
 	m_ef_weapon_type			= pSettings->r_u32(section,"ef_weapon_type");
+
+	DestroyAfterBlowout = READ_IF_EXISTS(pSettings, r_bool, section, "DestroyAfterBlowout", false);
 }
 
 BOOL CCustomZone::net_Spawn(CSE_Abstract* DC) 
@@ -604,6 +606,9 @@ void CCustomZone::shedule_Update(u32 dt)
 	  inherited::shedule_Update( dt );
 
 	UpdateOnOffState	();
+
+	if (LastBlowoutTime && (Device.dwTimeGlobal - LastBlowoutTime) > 300)
+		DestroyObject();
 }
 
 void CCustomZone::CheckForAwaking()
@@ -811,6 +816,9 @@ void CCustomZone::PlayBlowoutParticles()
 	m_pBlowoutParticles->UpdateParent( XFORM(), zero_vel );
 	if ( !m_pBlowoutParticles->IsPlaying() )
 	  m_pBlowoutParticles->Play();
+
+	if (DestroyAfterBlowout)
+		LastBlowoutTime = Device.dwTimeGlobal;
 }
 
 void CCustomZone::StopBlowoutParticles() {
