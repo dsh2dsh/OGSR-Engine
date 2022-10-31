57f11dcce3d12fb7d3d16d451ff0e6d284b5494f
Sergii T <5493488+joye-ramone@users.noreply.github.com>
2022-10-31 16:43:41 +0100

    PHObjectPositionUpdate на спавне помогает объектам найти себя
    Co-Authored-By: Sergii T <5493488+joye-ramone@users.noreply.github.com>

--- ogsr_engine/xrGame/PhysicObject.cpp
+++ ogsr_engine/xrGame/PhysicObject.cpp
@@ -53,6 +53,8 @@ BOOL CPhysicObject::net_Spawn(CSE_Abstract* DC)
     m_just_after_spawn = true;
     m_activated = false;
 
+    PHObjectPositionUpdate();
+
     // приложить небольшую силу для того, чтобы объект начал падать
     PPhysicsShell()->applyImpulse(Fvector().set(0.f, -1.0f, 0.f), 0.5f * PPhysicsShell()->getMass());
 
