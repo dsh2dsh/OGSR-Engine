85064f9d6f372a3799327adcf5ff0c3794772679
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-05-19 17:31:59 +0200

    Переформатировал

--- ogsr_engine/xrGame/console_commands.cpp
+++ ogsr_engine/xrGame/console_commands.cpp
@@ -72,7 +72,9 @@ extern BOOL g_bCopDeathAnim;
 
 BOOL g_bCheckTime = FALSE;
 int net_cl_inputupdaterate = 50;
-Flags32 g_mt_config = {mtLevelPath | mtDetailPath | mtObjectHandler | mtSoundPlayer | mtAiVision | mtBullets | mtLUA_GC | mtLevelSounds | mtALife | mtMap};
+Flags32 g_mt_config = {mtLevelPath | mtDetailPath | mtObjectHandler |
+                       mtSoundPlayer | mtAiVision | mtBullets | mtLUA_GC |
+                       mtLevelSounds | mtALife | mtMap};
 #ifdef DEBUG
 Flags32 dbg_net_Draw_Flags = {0};
 #endif
