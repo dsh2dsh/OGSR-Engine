f2f1982768fca7ba36ed2f0ba9841d230dd5adb0
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-07-04 16:28:12 +0200

    Добавил ASSERT_FMT

--- ogsr_engine/xr_3da/IGame_ObjectPool.cpp
+++ ogsr_engine/xr_3da/IGame_ObjectPool.cpp
@@ -12,12 +12,12 @@ void IGame_ObjectPool::prefetch()
 {
     R_ASSERT(m_PrefetchObjects.empty());
 
-    int p_count = 0;
     ::Render->model_Logging(FALSE);
 
     string256 section;
     // prefetch objects
-    strconcat(sizeof(section), section, "prefetch_objects_", g_pGamePersistent->m_game_params.m_game_type);
+    strconcat(sizeof(section), section, "prefetch_objects_",
+              g_pGamePersistent->m_game_params.m_game_type);
     CInifile::Sect& sect = pSettings->r_section(section);
     CTimer T;
     T.Start();
@@ -25,15 +25,19 @@ void IGame_ObjectPool::prefetch()
     for (const auto& item : sect.Data)
     {
         CLASS_ID CLS = pSettings->r_clsid(item.first.c_str(), "class");
-        p_count++;
         CObject* pObject = (CObject*)NEW_INSTANCE(CLS);
         pObject->Load(item.first.c_str());
         pObject->reload(item.first.c_str());
-        VERIFY2(pObject->cNameSect().c_str(), item.first.c_str());
+        ASSERT_FMT(
+            xr_strcmp(pObject->cNameSect().c_str(), item.first.c_str()) == 0,
+            "[%s]: unexpected section: '%s' != '%s', class[%s]",
+            pObject->cNameSect().c_str(), item.first.c_str(),
+            pSettings->r_string(item.first.c_str(), "class"));
         m_PrefetchObjects.push_back(pObject);
     }
     Render->models_begin_prefetch1(false);
-    Msg("[%s] objects prefetching time (%zi): [%.2f s.]", __FUNCTION__, p_count, T.GetElapsed_sec());
+    Msg("[%s] objects prefetching time (%u): [%.3f s.]", __FUNCTION__,
+        m_PrefetchObjects.size(), T.GetElapsed_sec());
 
     // out statistic
     ::Render->model_Logging(TRUE);
